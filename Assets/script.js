5f95a08969f247a2c52a266414c038605e99f1dd
Travis Puryear <travispuryear678@gmail.com>
2021-09-25 11:27:33 -0400

    When start is clicked it is removed and questions button show up.

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
 // Global vars.
 const startBtn = document.getElementById('startBtn');
-const questions = document.getElementById('Q');
+const questionsbox = document.getElementById('box')
 
 
 
@@ -9,10 +9,8 @@ startBtn.addEventListener("click", startQuiz);
 // When start clicked begins a timer for each questiuon and removes start button from view.
 function startQuiz(){
     console.log("Game begins");
-    startBtn.setAttribute('class', 'remove');
+    startBtn.classList.add('remove');
+    questionsbox.classList.remove('remove');
     
-    questions.removeAttribute('class', 'remove');
-
-
-
 }
+console.log(startQuiz);
