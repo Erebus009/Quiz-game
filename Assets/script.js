7e061bbb109242ba6d05d55944f32fb4be458cf4
Travis Puryear <travispuryear678@gmail.com>
2021-09-25 14:35:24 -0400

    querySelectors to add text to buttons and add in question.

--- Assets/script.js
+++ Assets/script.js
@@ -6,10 +6,18 @@ const questionsbox = document.getElementById('box')
 var count = document.querySelector('.timer')
 var question = document.querySelector('.questions')
 
-// object layout for questions. 
+//  layout for questions. 
 var questions = [
+
+    "What does the milk from cows produce?"
+
+
+]
+
+
+var answers = [
     "cheese",
-    "milk",
+    "things",
     "curds",
     "yogurt"
 ]
@@ -24,6 +32,24 @@ function startQuiz(){
     startBtn.classList.add('remove');
     // remove class remove on div box containing all the buttons and questions to begin question 1. 
     questionsbox.classList.remove('remove');
+    
+    // populate buttons with answers. 
+
+    var node = document.createTextNode(answers[0]);
+    document.querySelector('.answer1').appendChild(node);
+
+    var node2 = document.createTextNode(answers[1]);
+    document.querySelector('.answer2').appendChild(node2);
+    
+    var node3 = document.createTextNode(answers[2]);
+    document.querySelector('.answer3').appendChild(node3);
+    
+    var node4 = document.createTextNode(answers[3]);
+    document.querySelector('.answer4').appendChild(node4);
+   
+    var asker = document.createTextNode(questions[0]);
+    document.querySelector('.questions').appendChild(asker);
+    
 
     // If start button is clicked start timer for each question in array. 
     var time = 45;
@@ -42,6 +68,8 @@ function startQuiz(){
 
 }
 
+
+    
 
 
 
