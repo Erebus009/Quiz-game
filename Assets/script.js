ab349f885c80ab1ee2af5d9a92869c5411a504d0
Travis Puryear <travispuryear678@gmail.com>
2021-09-25 11:48:20 -0400

    intervat timer to start when start button is cllicked.

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,7 @@
 // Global vars.
 const startBtn = document.getElementById('startBtn');
 const questionsbox = document.getElementById('box')
+var count = document.querySelector('.Quiz-box')
 
 
 
@@ -9,8 +10,28 @@ startBtn.addEventListener("click", startQuiz);
 // When start clicked begins a timer for each questiuon and removes start button from view.
 function startQuiz(){
     console.log("Game begins");
+    // add class remove to start btn CSS= display:none;
     startBtn.classList.add('remove');
+    // remove class remove on div box containing all the buttons and questions to begin question 1. 
     questionsbox.classList.remove('remove');
+
+    // If button is clicked start timer for each question in array. 
+    var time = 60;
+    
+    var timer = setInterval(() => {
+        if ( time > 0) {
+            time--,
+        count.textContent = time;
+        }
+        
+    }, 1000);
+
     
 }
+
+
+
+
+
+
 console.log(startQuiz);
