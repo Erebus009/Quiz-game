4ea07250660bfe63980aef0559055c6396c5c2f3
Travis Puryear <travispuryear678@gmail.com>
2021-09-25 10:48:30 -0400

    Start btn adds remove class when clicked.

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,18 @@
 // Global vars.
 const startBtn = document.getElementById('startBtn');
+const questions = document.getElementById('Q');
 
 
-startBtn.addEventListener("click", startQuiz)
 
+startBtn.addEventListener("click", startQuiz);
 
+// When start clicked begins a timer for each questiuon and removes start button from view.
 function startQuiz(){
     console.log("Game begins");
+    startBtn.setAttribute('class', 'remove');
+    
+    questions.removeAttribute('class', 'remove');
+
+
 
 }
-
